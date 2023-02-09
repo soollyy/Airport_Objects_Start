1f2dd40770364676cb8af46aa2bdf3aab1c5a3b9
soollyy <solianatesema1@gmail.com>
2023-02-09 16:21:41 +0000

    half way

--- Person.js
+++ Person.js
@@ -4,14 +4,13 @@ class Person {
         this.name = name
         this.bags = bags
     }
-        // 1. Name
+        // 1. Name 
         // 2. Bags
             // 2a. Bags should contain/reference a data type for storing more than one bag object.
 
     // Include an addBag method
     addbag(bag) {
         this.bags.push(bag)
-        console.log("Adds a bag")
     }
 }   
 
